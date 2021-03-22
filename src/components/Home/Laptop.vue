48be13ae1f6b7368c062a687366b302824845d9b
denisov-k <denisov.k.o@yandex.ru>
2021-03-22 18:43:41 +0300

    Style fix

--- src/components/Home/Laptop.vue
+++ src/components/Home/Laptop.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="laptop">
     <img src="@/assets/laptop.svg">
+    <div class="laptop-screen"></div>
   </div>
 </template>
 
@@ -12,12 +13,23 @@ name: "Laptop"
 
 <style scoped>
   .laptop {
-    height: 72vh;
+    /*height: 72vh;*/
     display: flex;
+    position: relative;
     justify-content: center;
-    margin: 0 40px;
+    margin: 0 70px;
   }
   .laptop img {
-
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
+  .laptop-screen {
+    position: absolute;
+    left: 11.5%;
+    right: 11.5%;
+    top: 20px;
+    bottom: 66px;
+    background-color: #335cd2;
   }
 </style>
